e3c4d906a8968945f025beae86f6f584be595fbd
Erik Funder Carstensen <fundererik@fastmail.com>
2021-11-16 15:52:20 +0100

    [WIP] Add documentation to Frc methods

---
+++ src/frac.rs
@@ -0,0 +1,95 @@
+use std::fmt::Display;
+
+pub(crate) struct Frac {
+    num: usize,
+    den: usize,
+}
+
+impl Frac {
+    pub(crate) fn new() -> Self {
+        Frac { num: 1, den: 1 }
+    }
+
+    pub(crate) fn split(&mut self) -> Result<Self, SplitErr> {
+        self.den = self.den.checked_add(1).ok_or(SplitErr())?;
+        Ok(Self {
+            num: self.num,
+            den: self.den,
+        })
+    }
+
+    pub(crate) fn merge(&mut self, mut other: Self) -> Result<(), MergeErr> {
+        let (min_den, max_den) = if self.den < other.den {
+            (&mut *self, &mut other)
+        } else {
+            (&mut other, &mut *self)
+        };
+
+        let diff = max_den.den - min_den.den;
+        min_den.num = min_den
+            .num
+            .checked_shl(diff.try_into().map_err(|_| MergeErr())?)
+            .ok_or(MergeErr())?;
+        let mut num = min_den.num + max_den.num;
+        let trailing = num.trailing_zeros() as usize;
+        num >>= trailing;
+        let den = max_den.den - trailing;
+
+        self.num = num;
+        self.den = den;
+        Ok(())
+    }
+
+    pub(crate) fn is_one(&self) -> bool {
+        self.num == 1 && self.den == 1
+    }
+}
+
+impl std::fmt::Debug for Frac {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}/((2^{}) - 1)", self.num, self.den)
+    }
+}
+
+#[derive(Debug)]
+pub struct MergeErr();
+
+impl Display for MergeErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Nominator became too big, could not merge!")
+    }
+}
+
+impl std::error::Error for MergeErr {}
+
+#[derive(Debug)]
+pub struct SplitErr();
+
+impl Display for SplitErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Denominater was about to overflow, could not split!")
+    }
+}
+impl std::error::Error for SplitErr {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_is_one() {
+        let frac = Frac::new();
+        assert!(frac.is_one());
+    }
+
+    #[test]
+    fn split_seems_reasonable() {
+        let mut three_fourth = Frac::new();
+        let mut half = three_fourth.split().unwrap();
+        let quater = half.split().unwrap();
+        three_fourth.merge(quater);
+
+        assert_eq!(3, three_fourth.num);
+        assert_eq!(4, 1 << (three_fourth.den - 1));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,72 +1,11 @@
-use std::{fmt::Display, marker::PhantomData, ops::Deref, ptr::NonNull};
+mod frac;
 
 use frac::Frac;
 
-mod frac {
-    pub(crate) struct Frac {
-        num: usize,
-        den: usize,
-    }
+use std::{fmt::Display, marker::PhantomData, ops::Deref, ptr::NonNull};
 
-    impl Frac {
-        pub(crate) fn new() -> Self {
-            Frac { num: 1, den: 1 }
-        }
-
-        pub(crate) fn split(&mut self) -> Self {
-            self.den += 1;
-            Self {
-                num: self.num,
-                den: self.den,
-            }
-        }
-
-        pub(crate) fn merge(&mut self, mut other: Self) {
-            let (min_den, max_den) = if self.den < other.den {
-                (&mut *self, &mut other)
-            } else {
-                (&mut other, &mut *self)
-            };
-
-            let diff = max_den.den - min_den.den;
-            min_den.num <<= diff;
-            let mut num = min_den.num + max_den.num;
-            let trailing = num.trailing_zeros() as usize;
-            num >>= trailing;
-            let den = max_den.den - trailing;
-
-            self.num = num;
-            self.den = den;
-        }
-
-        pub(crate) fn is_one(&self) -> bool {
-            self.num == 1 && self.den == 1
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn new_is_one() {
-            let frac = Frac::new();
-            assert!(frac.is_one());
-        }
-
-        #[test]
-        fn split_seems_reasonable() {
-            let mut three_fourth = Frac::new();
-            let mut half = three_fourth.split();
-            let quater = half.split();
-            three_fourth.merge(quater);
-
-            assert_eq!(3, three_fourth.num);
-            assert_eq!(4, 1 << (three_fourth.den - 1));
-        }
-    }
-}
-
+#[derive(Debug)]
+// TODO: Custom Debug implementation
 pub struct Frc<T: ?Sized> {
     item: NonNull<T>,
     frac: Frac,
@@ -86,28 +25,84 @@ impl<T: ?Sized> Frc<T> {
 }
 
 impl<T> Frc<T> {
+    /// Creates a new [Frc] by boxing the input and initializing ownership fraction.
     pub fn new(v: T) -> Self {
         Box::new(v).into()
     }
 
+    /// Returns the item behind the [Frc] if all splits of the [Frc] have been merged. Otherwise returns the [Frc].
+    ///
+    /// # Examples
+    /// ```
+    /// use frc::Frc;
+    ///
+    /// let mut first = Frc::new(8);
+    /// let mut split = first.split();
+    ///
+    /// let mut split = match split.try_unwrap() {
+    ///     Err(s) => s,
+    ///     _ => panic!(),
+    /// };
+    ///
+    /// split.merge(first);
+    ///
+    /// let inner = split.try_unwrap();
+    ///
+    /// assert!(inner.is_ok());
+    /// assert_eq!(inner.unwrap(), 8);
+    /// ```
     pub fn try_unwrap(self) -> Result<T, Self> {
         if self.frac.is_one() {
-            Ok(*unsafe { Box::from_raw(self.item.as_ptr()) })
+            Ok(unsafe { self.unwrap_unchecked() })
         } else {
             Err(self)
         }
     }
 
+    /// Unwraps the [Frc] without checking if all fractions have been merged into it.
+    ///
+    /// # Safety
+    /// There may not exist any other [Frc] containing the same data
+    ///
+    /// # Examples
+    /// ```
+    /// use frc::Frc;
+    ///
+    /// let mut f = Frc::new(8);
+    /// let mut split = f.split();
+    /// let third = split.split();
+    /// // let inner = unsafe {split.unwrap_unchecked()}; // This is UB
+    /// drop(f);
+    /// // let inner = unsafe {split.unwrap_unchecked()}; // This is UB
+    /// drop(third);
+    ///
+    /// let inner = unsafe {split.unwrap_unchecked()}; // Ok, since no other Frc referincing the data exists
+    /// ```
+    pub unsafe fn unwrap_unchecked(self) -> T {
+        *Box::from_raw(self.item.as_ptr())
+    }
+
+    /// Creates a [Frc], distributing the ownership of the input between itself and the new [Frc].
+    ///
+    /// # Examples
+    /// ```
+    /// use frc::Frc;
+    ///
+    /// let mut first = Frc::new("Abra cadabra");
+    /// let second = first.split();
+    /// assert_eq!(*first, *second);
+    /// ```
     pub fn split(&mut self) -> Self {
         Self {
             item: self.item,
-            frac: self.frac.split(),
+            frac: self.frac.split().unwrap(),
             phantom: self.phantom,
         }
     }
 
     pub unsafe fn merge_unchecked(&mut self, other: Self) {
-        self.frac.merge(other.frac);
+        // Silently fails if
+        let _ = self.frac.merge(other.frac);
     }
 
     pub fn try_merge(&mut self, other: Self) -> Result<(), MergeErr> {
@@ -153,8 +148,5 @@ impl std::error::Error for MergeErr {}
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    fn single_threaded() {}
 }
